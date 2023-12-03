b7a8fae64e2149dc450316124e22353d37e346ab
prateek692 <prateek692@hotmail.com>
2023-12-03 17:20:10 +0530

    New Data and Script for graphs

--- graph.py
+++ graph.py
@@ -4,49 +4,167 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-# Read the .testlog file into a list of lines
 with open('optdigits.test.csv_mart_J20_v0.1.testlog', 'r') as file:
     lines = file.readlines()
-
-# Split each line into columns based on the delimiter
 data = [re.split(r'\s+', line.strip()) for line in lines]
-
-# Create a DataFrame using pandas
 mart = pd.DataFrame(data)
-
 with open('optdigits.test.csv_robustlogit_J20_v0.1.testlog', 'r') as file:
     lines = file.readlines()
-
-# Split each line into columns based on the delimiter
 data = [re.split(r'\s+', line.strip()) for line in lines]
-
-# Create a DataFrame using pandas
 robustlogit = pd.DataFrame(data)
-
-
 # Errors for both method
-errors_mart=mart[2].to_numpy().astype(int)
-errors_robustlogit=robustlogit[2].to_numpy().astype(int)
-
-
-
-
-x_values = np.arange(1, len(errors_mart) + 1)
-
+errors_mart_optdigits=mart[2].to_numpy().astype(int)
+errors_robustlogit_optdigits=robustlogit[2].to_numpy().astype(int)
+x_values = np.arange(1, len(errors_mart_optdigits) + 1)
 # Plot the graph
-plt.plot(x_values, errors_mart,  linestyle='-', color='b', label='MART')
-plt.plot(x_values, errors_robustlogit, linestyle='-', color='r', label='Robust LogitBoost')
+plt.plot(x_values, errors_mart_optdigits,  linestyle='-', color='b', label='MART')
+plt.plot(x_values, errors_robustlogit_optdigits, linestyle='-', color='r', label='Robust LogitBoost')
 # Add labels and title
 plt.xlabel('Iterations')
 plt.ylabel('Errors')
-plt.title('Graph of Iterations vs Error')
-
-
+plt.title('Optdigits Dataset')
 # Add legend
 plt.legend()
-
 # Show the plot
-plt.savefig('graph.png') 
-# plt.show()
+plt.savefig('graph_optdigits.png') 
+plt.close()
 
 
+
+
+with open('comp_cpu.test.csv_mart_J20_v0.1.testlog', 'r') as file:
+    lines = file.readlines()
+data = [re.split(r'\s+', line.strip()) for line in lines]
+mart = pd.DataFrame(data)
+with open('comp_cpu.test.csv_robustlogit_J20_v0.1.testlog', 'r') as file:
+    lines = file.readlines()
+data = [re.split(r'\s+', line.strip()) for line in lines]
+robustlogit = pd.DataFrame(data)
+# Errors for both method
+errors_mart_comp_cpu=mart[2].to_numpy().astype(int)
+errors_robustlogit_comp_cpu=robustlogit[2].to_numpy().astype(int)
+x_values = np.arange(1, len(errors_mart_comp_cpu) + 1)
+# Plot the graph
+plt.plot(x_values, errors_mart_comp_cpu,  linestyle='-', color='b', label='MART')
+plt.plot(x_values, errors_robustlogit_comp_cpu, linestyle='-', color='r', label='Robust LogitBoost')
+# Add labels and title
+plt.xlabel('Iterations')
+plt.ylabel('Errors')
+plt.title('Comp_CPU Dataset')
+# Add legend
+plt.legend()
+# Show the plot
+plt.savefig('graph_compcpu.png') 
+plt.close()
+
+with open('covtype.test.csv_mart_J20_v0.1.testlog', 'r') as file:
+    lines = file.readlines()
+data = [re.split(r'\s+', line.strip()) for line in lines]
+mart = pd.DataFrame(data)
+with open('covtype.test.csv_robustlogit_J20_v0.1.testlog', 'r') as file:
+    lines = file.readlines()
+data = [re.split(r'\s+', line.strip()) for line in lines]
+robustlogit = pd.DataFrame(data)
+# Errors for both method
+errors_mart_covtype=mart[2].to_numpy().astype(int)
+errors_robustlogit_covtype=robustlogit[2].to_numpy().astype(int)
+x_values = np.arange(1, len(errors_mart_covtype) + 1)
+# Plot the graph
+plt.plot(x_values, errors_mart_covtype,  linestyle='-', color='b', label='MART')
+plt.plot(x_values, errors_robustlogit_covtype, linestyle='-', color='r', label='Robust LogitBoost')
+# Add labels and title
+plt.xlabel('Iterations')
+plt.ylabel('Errors')
+plt.title('Covtype Dataset')
+# Add legend
+plt.legend()
+# Show the plot
+plt.savefig('graph_covtype.png') 
+plt.close()
+
+with open('ijcnn1.test.csv_mart_J20_v0.1.testlog', 'r') as file:
+    lines = file.readlines()
+data = [re.split(r'\s+', line.strip()) for line in lines]
+mart = pd.DataFrame(data)
+with open('ijcnn1.test.csv_robustlogit_J20_v0.1.testlog', 'r') as file:
+    lines = file.readlines()
+data = [re.split(r'\s+', line.strip()) for line in lines]
+robustlogit = pd.DataFrame(data)
+# Errors for both method
+errors_mart_ijcnn=mart[2].to_numpy().astype(int)
+errors_robustlogit_ijcnn=robustlogit[2].to_numpy().astype(int)
+x_values = np.arange(1, len(errors_mart_ijcnn) + 1)
+# Plot the graph
+plt.plot(x_values, errors_mart_ijcnn,  linestyle='-', color='b', label='MART')
+plt.plot(x_values, errors_robustlogit_ijcnn, linestyle='-', color='r', label='Robust LogitBoost')
+# Add labels and title
+plt.xlabel('Iterations')
+plt.ylabel('Errors')
+plt.title('IJCNN1 Dataset')
+# Add legend
+plt.legend()
+# Show the plot
+plt.savefig('graph_ijcnn1.png') 
+plt.close()
+
+
+
+
+
+with open('pendigits.test.csv_mart_J20_v0.1.testlog', 'r') as file:
+    lines = file.readlines()
+data = [re.split(r'\s+', line.strip()) for line in lines]
+mart = pd.DataFrame(data)
+with open('pendigits.test.csv_robustlogit_J20_v0.1.testlog', 'r') as file:
+    lines = file.readlines()
+data = [re.split(r'\s+', line.strip()) for line in lines]
+robustlogit = pd.DataFrame(data)
+# Errors for both method
+errors_mart_pendigits=mart[2].to_numpy().astype(int)
+errors_robustlogit_pendigits=robustlogit[2].to_numpy().astype(int)
+x_values = np.arange(1, len(errors_mart_pendigits) + 1)
+# Plot the graph
+plt.plot(x_values, errors_mart_pendigits,  linestyle='-', color='b', label='MART')
+plt.plot(x_values, errors_robustlogit_pendigits, linestyle='-', color='r', label='Robust LogitBoost')
+# Add labels and title
+plt.xlabel('Iterations')
+plt.ylabel('Errors')
+plt.title('Pendigits Dataset')
+# Add legend
+plt.legend()
+# Show the plot
+plt.savefig('graph_pendigits.png') 
+plt.close()
+
+
+#MART
+plt.plot(x_values, errors_mart_pendigits,  linestyle='-', color='b', label='Pendigits')
+plt.plot(x_values, errors_mart_comp_cpu, linestyle='-', color='r', label='Comp_CPU')
+plt.plot(x_values, errors_mart_covtype,  linestyle='-', color='g', label='Covtype')
+plt.plot(x_values, errors_mart_optdigits, linestyle='-', color='y', label='Optdigits')
+plt.plot(x_values, errors_mart_ijcnn,  linestyle='-', color='k', label='IJCNN')
+# Add labels and title
+plt.xlabel('Iterations')
+plt.ylabel('Errors')
+plt.title('MART Across different Datasets')
+# Add legend
+plt.legend()
+# Show the plot
+plt.savefig('graph_mart.png') 
+plt.close()
+
+#ROBUST
+plt.plot(x_values, errors_robustlogit_pendigits,  linestyle='-', color='b', label='Pendigits')
+plt.plot(x_values, errors_robustlogit_comp_cpu, linestyle='-', color='r', label='Comp_CPU')
+plt.plot(x_values, errors_robustlogit_covtype,  linestyle='-', color='g', label='Covtype')
+plt.plot(x_values, errors_robustlogit_optdigits, linestyle='-', color='y', label='Optdigits')
+plt.plot(x_values, errors_robustlogit_ijcnn,  linestyle='-', color='k', label='IJCNN')
+# Add labels and title
+plt.xlabel('Iterations')
+plt.ylabel('Errors')
+plt.title('Robust LogitBoost Across different Datasets')
+# Add legend
+plt.legend()
+# Show the plot
+plt.savefig('graph_robustlogit.png') 
+plt.close()
--- script.sh
+++ script.sh
@@ -4,15 +4,47 @@ cmake -DOMP=ON -DNATIVE=ON ..
 make
 cd ..
 # Training
-./abcboost_train -method robustlogit -data ./data/optdigits.train.csv -v 0.1 -J 20 -iter 100 -feature_split_sample_rate 0.1
-./abcboost_train -method mart -data ./data/optdigits.train.csv -v 0.1 -J 20 -iter 100 -feature_split_sample_rate 0.1
+./abcboost_train -method robustlogit -data ./data/comp_cpu.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method mart -data ./data/comp_cpu.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method robustlogit -data ./data/covtype.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method mart -data ./data/covtype.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method robustlogit -data ./data/ijcnn1.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method mart -data ./data/ijcnn1.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method robustlogit -data ./data/optdigits.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method mart -data ./data/optdigits.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method robustlogit -data ./data/pendigits.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
+./abcboost_train -method mart -data ./data/pendigits.train.csv -v 0.1 -J 20 -iter 5 -feature_split_sample_rate 0.1
 # Unlearning
 echo 9 > unids.txt # Unlearn 9-th data sample
+./abcboost_unlearn -data ./data/comp_cpu.train.csv -model comp_cpu.train.csv_robustlogit_J20_v0.1.model -unlearning_ids_path unids.txt
+./abcboost_unlearn -data ./data/comp_cpu.train.csv -model comp_cpu.train.csv_mart_J20_v0.1.model -unlearning_ids_path unids.txt
+./abcboost_unlearn -data ./data/covtype.train.csv -model covtype.train.csv_robustlogit_J20_v0.1.model -unlearning_ids_path unids.txt
+./abcboost_unlearn -data ./data/covtype.train.csv -model covtype.train.csv_mart_J20_v0.1.model -unlearning_ids_path unids.txt
+./abcboost_unlearn -data ./data/ijcnn1.train.csv -model ijcnn1.train.csv_robustlogit_J20_v0.1.model -unlearning_ids_path unids.txt
+./abcboost_unlearn -data ./data/ijcnn1.train.csv -model ijcnn1.train.csv_mart_J20_v0.1.model -unlearning_ids_path unids.txt
 ./abcboost_unlearn -data ./data/optdigits.train.csv -model optdigits.train.csv_robustlogit_J20_v0.1.model -unlearning_ids_path unids.txt
 ./abcboost_unlearn -data ./data/optdigits.train.csv -model optdigits.train.csv_mart_J20_v0.1.model -unlearning_ids_path unids.txt
+./abcboost_unlearn -data ./data/pendigits.train.csv -model pendigits.train.csv_robustlogit_J20_v0.1.model -unlearning_ids_path unids.txt
+./abcboost_unlearn -data ./data/pendigits.train.csv -model pendigits.train.csv_mart_J20_v0.1.model -unlearning_ids_path unids.txt
 #Prediction
+./abcboost_predict -data ./data/comp_cpu.test.csv -model comp_cpu.train.csv_robustlogit_J20_v0.1.model
+./abcboost_predict -data ./data/comp_cpu.test.csv -model comp_cpu.train.csv_robustlogit_J20_v0.1_unlearn.model
+./abcboost_predict -data ./data/comp_cpu.test.csv -model comp_cpu.train.csv_mart_J20_v0.1.model
+./abcboost_predict -data ./data/comp_cpu.test.csv -model comp_cpu.train.csv_mart_J20_v0.1_unlearn.model
+./abcboost_predict -data ./data/covtype.test.csv -model covtype.train.csv_robustlogit_J20_v0.1.model
+./abcboost_predict -data ./data/covtype.test.csv -model covtype.train.csv_robustlogit_J20_v0.1_unlearn.model
+./abcboost_predict -data ./data/covtype.test.csv -model covtype.train.csv_mart_J20_v0.1.model
+./abcboost_predict -data ./data/covtype.test.csv -model covtype.train.csv_mart_J20_v0.1_unlearn.model
+./abcboost_predict -data ./data/ijcnn1.test.csv -model ijcnn1.train.csv_robustlogit_J20_v0.1.model
+./abcboost_predict -data ./data/ijcnn1.test.csv -model ijcnn1.train.csv_robustlogit_J20_v0.1_unlearn.model
+./abcboost_predict -data ./data/ijcnn1.test.csv -model ijcnn1.train.csv_mart_J20_v0.1.model
+./abcboost_predict -data ./data/ijcnn1.test.csv -model ijcnn1.train.csv_mart_J20_v0.1_unlearn.model
 ./abcboost_predict -data ./data/optdigits.test.csv -model optdigits.train.csv_robustlogit_J20_v0.1.model
 ./abcboost_predict -data ./data/optdigits.test.csv -model optdigits.train.csv_robustlogit_J20_v0.1_unlearn.model
 ./abcboost_predict -data ./data/optdigits.test.csv -model optdigits.train.csv_mart_J20_v0.1.model
 ./abcboost_predict -data ./data/optdigits.test.csv -model optdigits.train.csv_mart_J20_v0.1_unlearn.model
+./abcboost_predict -data ./data/pendigits.test.csv -model pendigits.train.csv_robustlogit_J20_v0.1.model
+./abcboost_predict -data ./data/pendigits.test.csv -model pendigits.train.csv_robustlogit_J20_v0.1_unlearn.model
+./abcboost_predict -data ./data/pendigits.test.csv -model pendigits.train.csv_mart_J20_v0.1.model
+./abcboost_predict -data ./data/pendigits.test.csv -model pendigits.train.csv_mart_J20_v0.1_unlearn.model
 python graph.py
